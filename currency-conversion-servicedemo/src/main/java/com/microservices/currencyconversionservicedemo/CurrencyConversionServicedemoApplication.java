6fa54a908c635c4ad637e99f7e4b241320c57179
Andrei-Georgian Craciun <acraciun@deloittece.com>
2023-09-04 16:28:52 +0300

    Setting up Currency Conversion Microservice + Setting up Currency Exchange Microservice

---
+++ currency-conversion-servicedemo/src/main/java/com/microservices/currencyconversionservicedemo/CurrencyConversionServicedemoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.currencyconversionservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServicedemoApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-servicedemo/src/test/java/com/microservices/currencyconversionservicedemo/CurrencyConversionServicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.currencyconversionservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServicedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
